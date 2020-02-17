d85325d18083250807d9a7b7e36ced145eec6541
Graytwentythree <graytwentythree@github.com>
2020-02-16 19:30:28 -0500

    created staff tables [WIP] foreign key issue

--- staff_init.sql
+++ staff_init.sql
@@ -4,9 +4,53 @@
 * Initialize Staff tables 
 */
 
-USE HOSPITAL;
 
-CREATE TABLE IF NOT EXISTS WARD (
-	WardID SMALLINT,
-	WardName VARCHAR(60)
+USE HOSPITAL;
+	
+CREATE TABLE IF NOT EXISTS Ward (
+	WardID SMALLINT NOT NULL,
+	WardName VARCHAR(64),
+    TotalBeds SMALLINT,
+    BedsInUse SMALLINT,
+    PRIMARY KEY (WardID)
 );
+
+CREATE TABLE IF NOT EXISTS Staff (
+	StaffID SMALLINT NOT NULL,
+    StaffLastName VARCHAR(64) NOT NULL,
+    StaffFirstName VARCHAR(64) NOT NULL,
+    JobTitle VARCHAR(128),
+    PRIMARY KEY (StaffID),
+    FOREIGN KEY (WardID) REFERENCES Ward(WardID)
+);
+
+CREATE TABLE IF NOT EXISTS Jobs (
+	JobID SMALLINT NOT NULL,
+    JobName VARCHAR(128),
+    Opennings SMALLINT,
+    PRIMARY KEY (JobID)
+);
+CREATE TABLE IF NOT EXISTS Certifications (
+	FOREIGN KEY(StaffID) REFERENCES Staff(StaffID),
+    Cert VARCHAR(64),
+    Completed DATE,
+    PRIMARY KEY (StaffID, Completed)
+);
+CREATE TABLE IF NOT EXISTS EmploymentExperience (
+	FOREIGN KEY(StaffID) REFERENCES Staff(StaffID),
+    Job VARCHAR(64),
+    StartDate DATE,
+    EndDate DATE,
+    PRIMARY KEY(StaffID, EndDate)
+);
+/*
+* Not sure if this goes here. *Figure out later*
+CREATE TABLE IF NOT EXISTS LocalDoctors (
+	ClinicID SMALLINT,
+    ClinicName VARCAR(128),
+    DoctorID SMALLINT,
+    DoctorLastName VARCHAR(64),
+    DoctorFirstName VARCHAR(64),
+    PRIMARY KEY (DoctorID)
+);
+*/
